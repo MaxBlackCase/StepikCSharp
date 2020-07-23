6f37240e9f4cd8b7eeb3b031263975eeef9ea75e
Max Black <MaxBlackCase@gmail.com>
2020-07-23 13:13:18 +0400

    Проекст с явным и сылочным типом взаимодействия с Объектом

---
+++ RefTypeValTypeParams/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RefTypeValTypeParams
+{
+  internal class Person
+  {
+    public string personName;
+    public int personAge;
+    public Person(string name, int age)
+    {
+
+      personName = name;
+      personAge = age;
+
+    }
+    public Person() { }
+
+    public void Display() {
+
+      Console.WriteLine("Name: {0}, Age: {1}", personName, personAge);  
+
+    }
+
+    
+  }
+}
---
+++ RefTypeValTypeParams/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RefTypeValTypeParams
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("/n***Новый экземпляр класса Person***/n");
+      Person fred = new Person("fred", 12);
+      fred.Display();
+
+      Console.WriteLine("/n***Переопределение экземпляра класса Person***/n");
+      SendAPersonByValue(fred);
+      fred.Display();
+      Console.WriteLine("/n***Переопределение экземпляра класса Person по ссылке (ref)***/n");
+      SendAPersonByReference(ref fred);
+      fred.Display();
+
+      Console.ReadLine();
+    }
+
+    static void SendAPersonByValue(Person p)
+    {
+      p.personAge = 99;
+      p = new Person("Andru", 99);
+    }
+
+    static void SendAPersonByReference(ref Person p)
+    {
+      p.personAge = 555;
+
+      p = new Person("Nikki", 999);
+    }
+  }
+}

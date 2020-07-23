97a59e0ffd716e27111c677001fdafb7d77fd4d4
Max Black <MaxBlackCase@gmail.com>
2020-07-23 11:09:47 +0400

    Создан простенький контруктор Point

---
+++ Struct/Point.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+  internal struct Point
+  {
+
+    public int X;
+    public int Y;
+
+    public void Increment()
+    {
+      X++; Y++;
+    }
+
+    public void Decrement()
+    {
+      X--; Y--;
+    }
+
+    public void Display()
+    {
+      Console.WriteLine("X: {0}, Y: {1}", X, Y);
+    }
+
+  }
+}
---
+++ Struct/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+
+      Point _point;
+      _point.X = 15;
+      _point.Y = 562;
+
+      _point.Display();
+      _point.Decrement();
+      _point.Display();
+
+      Console.ReadLine();
+
+    }
+  }
+}

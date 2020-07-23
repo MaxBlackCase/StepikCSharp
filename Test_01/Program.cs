d23775431a250afb8161d035228332b4aed8282d
Max Black <MaxBlackCase@gmail.com>
2020-07-23 10:47:51 +0400

    Создан еннумератор EnmType  и проест Test_01

---
+++ Test_01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_01
+{
+  internal class Program
+  {
+    static void Main()
+    {
+
+      EnmType enm = EnmType.VicePresident;
+      GetTypeEnm(enm);
+
+
+      Console.ReadLine();
+    }
+
+    private static void GetTypeEnm(EnmType e)
+    {
+      switch (e)
+      {
+        case EnmType.VicePresident:
+          Console.WriteLine($"Вы {EnmType.VicePresident}");
+          break;
+        case EnmType.Manager:
+          Console.WriteLine($"Вы {EnmType.Manager}");
+          break;
+        case EnmType.Contractor:
+          Console.WriteLine($"Вы {EnmType.Contractor}");
+          break;
+      }
+    }
+  }
+}

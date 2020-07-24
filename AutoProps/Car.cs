bb358729b35078dcd913fdb063c2b5837346971c
Max Black <maxblackcase@gmail.com>
2020-07-24 14:02:46 +0400

    добавлен проект AutoProps

---
+++ AutoProps/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AutoProps
+{
+  class Car
+  {
+
+    public string PetName { get; set; }
+    public int Speed { get; set; }
+    public string CarColor { get; set; }
+
+
+    public void DisplayStats()
+    {
+      Console.Write("Name: {0}\nSpeed: {1}\nColor: \n", PetName, Speed, CarColor);
+    }
+  }
+}
---
+++ AutoProps/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoProps
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      Car c = new Car();
+
+      c.PetName = "BMW";
+      c.Speed = 120;
+
+      c.DisplayStats();
+
+
+      Console.ReadLine();
+    }
+  }
+}
